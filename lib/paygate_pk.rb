58a2ffeeaf83aed22bc9ce006fbc1fb2170e43cb
Talha Junaid <talhajunaid65@gmail.com>
2025-10-01 14:22:00 +0500

    Initial Commit

---
+++ lib/paygate_pk.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "paygate_pk/version"
+
+module PaygatePk
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/paygate_pk/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module PaygatePk
+  VERSION = "0.1.0"
+end
